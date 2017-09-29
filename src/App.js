cf33aefd6583c641e902af631a54a0719fb80f9c
Trevor Schwab <trevschwab@gmail.com>
2017-09-29 15:27:04 -0500

    reacts essence

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://time-tracker-d3502.firebaseio.com/time/-KvDoHvUXL3b8oVGqenF.json?shallow=true')
+    .then((response) => response.json())
+    .then((response) => {
+      this.setState({
+        years: response
+      });
+    });
+  }
+
+  renderYears() {
+    return Object.keys(this.state.years).map((item) => (
+      <div key={item}>{item}</div>
+    ));
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Time Tracker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>{this.renderYears()}</div>
       </div>
     );
   }
